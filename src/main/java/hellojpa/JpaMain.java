aa37a9ada3b86da18edef13ce4b5d1dc26d9e986
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2022-09-21 22:03:04 +0900

    entity 삽입

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,40 +1,39 @@
-package hellojpa;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.List;
-
-public class JpaMain {
-
-    public static void main(String[] args) {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
-        EntityManager em = emf.createEntityManager();
-
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try {
-
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAA");
-
-            em.detach(member);
-
-            System.out.println("=========");
-            tx.commit();
-        }catch (Exception e){
-            tx.rollback();
-        }finally {
-            em.close();
-        }
-        emf.close();
-
-
-
-    }
-}
+//package hellojpa;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.EntityTransaction;
+//import javax.persistence.Persistence;
+//import java.util.List;
+//
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//
+//        EntityManager em = emf.createEntityManager();
+//
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//
+//        try {
+//
+//            //영속
+//            Member member = new Member();
+//            member.setId(3L);
+//            member.setUsername("USERTEST");
+//            member.setRoleType(RoleType.USER);
+//            em.persist(member);
+//            tx.commit();
+//        }catch (Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+//        emf.close();
+//
+//
+//
+//    }
+//}
